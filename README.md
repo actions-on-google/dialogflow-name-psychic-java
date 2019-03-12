a69068c2d749836cd544188499560f0dc8ac1519
Sarah Dwyer <dwyers@google.com>
2019-03-12 14:18:51 -0700

    Removes precise location check, README updates
    Bug: 127881951
    Change-Id: Ic22330d555d0499673721adbb5b908b1d4c10e10

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Actions on Google: Name Psychic Sample using Java
+# Actions on Google: Name Psychic Sample
 
-The Psychic in this sample can guess interesting secrets about you, like your
-name and where you are. This sample introduces permissions requests for user
-information on the Actions on Google platform using Java.
+This Java sample introduces permission requests for [user information](https://developers.google.com/actions/assistant/helpers#user_information) and
+demonstrates [surface transfer capabilities](https://developers.google.com/actions/assistant/surface-capabilities#multi-surface_conversations)
+when building Actions for Google Assistant and uses Google Maps Static API.
 
 ### Setup Instructions
+**This sample uses Google Maps Static API and so requires [Pay as You Go](https://developers.google.com/maps/documentation/maps-static/usage-and-billing) billing enabled via the Google Cloud Platform console under your project.**
 
 #### Action Configuration
-1. From the [Actions on Google Console](https://console.actions.google.com/), add a new project (this will become your *Project ID*) > **Create Project**.
-1. Scroll down to the **More Options** section, and click on the **Conversational** card.
+1. From the [Actions on Google Console](https://console.actions.google.com/), add a new project (this will become your *Project ID*) > **Create Project** > under **More options** > **Conversational** card.
 1. From the left navigation menu under **Build** > **Actions** > **Add Your First Action** > **BUILD** (this will bring you to the Dialogflow console) > Select language and time zone > **CREATE**.
 1. In Dialogflow, go to **Settings** ⚙ > **Export and Import** > **Restore from zip**.
     + Follow the directions to restore from the `agent.zip` file in this repo.
@@ -16,17 +16,17 @@ information on the Actions on Google platform using Java.
 #### App Engine Deployment & Webhook Configuration
 When a new project is created using the Actions Console, it also creates a Google Cloud project in the background.
 
-1. Obtain an API Key for the Google Maps Geocoding API following Step 1 of the instructions from [this page](https://developers.google.com/maps/documentation/geocoding/get-api-key).
-1. Reaplce `<YOUR_MAPS_KEY_HERE>` in `src/main/resources/config.properties` with your API Key for the Google Maps API.
-1. In the [Google Cloud Console API Library](https://console.cloud.google.com/apis/library), enable the Static Maps API for your project.
+1. Obtain an API key through [Google Cloud Platform console](https://console.cloud.google.com) under your *Project ID*, **Menu ☰** > **APIs & Services** > **Credentials** > **Create Credentials** > **API key**
+1. Replace `<YOUR_MAPS_KEY_HERE>` in `src/main/resources/config.properties` with the API key.
+1. In the [Google Cloud Console API Library](https://console.cloud.google.com/apis/library), enable the Static Maps API under your *Project ID*.
 1. Download & install the [Google Cloud SDK](https://cloud.google.com/sdk/docs/)
 1. Configure the gcloud CLI and set your Google Cloud project to the name of your Actions on Google Project ID, which you can find from the [Actions on Google console](https://console.actions.google.com/) under Settings ⚙
     + `gcloud init`
     + `gcloud auth application-default login`
     + `gcloud components install app-engine-java`
-    + `gcloud components update` 
-1. Deploy to [App Engine using Gradle](https://cloud.google.com/appengine/docs/flexible/java/using-gradle): 
-    + `gradle appengineDeploy` OR 
+    + `gcloud components update`
+1. Deploy to [App Engine using Gradle](https://cloud.google.com/appengine/docs/flexible/java/using-gradle):
+    + `gradle appengineDeploy` OR
     +  From within IntelliJ, open the Gradle tray and run the appEngineDeploy task.
 1. Back in the [Dialogflow console](https://console.dialogflow.com), from the left navigation menu under **Fulfillment** > **Enable Webhook**, set the value of **URL** to `https://<YOUR_PROJECT_ID>.appspot.com` > **Save**.
 
@@ -40,6 +40,7 @@ When a new project is created using the Actions Console, it also creates a Googl
 + For Actions on Google [documentation](https://developers.google.com/actions/).
 + For specifics about [Gradle & the App Engine Plugin](https://cloud.google.com/appengine/docs/flexible/java/using-gradle).
 + For details on deploying [Java apps with App Engine](https://cloud.google.com/appengine/docs/standard/java/quickstart).
++ To learn more about [Google Maps Static API](https://developers.google.com/maps/documentation/maps-static/intro).
 
 ### Make Contributions
 Please read and follow the steps in the [CONTRIBUTING.md](CONTRIBUTING.md).
--- src/main/java/com/example/MyActionsApp.java
+++ src/main/java/com/example/MyActionsApp.java
@@ -26,13 +26,8 @@ import com.google.actions.api.response.ResponseBuilder;
 import com.google.actions.api.response.helperintent.NewSurface;
 import com.google.actions.api.response.helperintent.Permission;
 import com.google.api.services.actions_fulfillment.v2.model.Image;
-import com.google.api.services.actions_fulfillment.v2.model.LatLng;
 import com.google.api.services.actions_fulfillment.v2.model.Surface;
-import com.google.maps.GeoApiContext;
-import com.google.maps.GeocodingApi;
-import com.google.maps.model.AddressComponent;
-import com.google.maps.model.AddressComponentType;
-import com.google.maps.model.GeocodingResult;
+
 import java.util.Collections;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -87,9 +82,10 @@ public class MyActionsApp extends DialogflowApp {
     String storageKey = STORAGE_KEY_NAME;
 
     if (!request.getUserStorage().containsKey(storageKey)) {
-      Permission permission = new Permission()
-          .setContext(formatResponse("permission_reason"))
-          .setPermissions(new String[]{requestedPermission});
+      Permission permission =
+          new Permission()
+              .setContext(formatResponse("permission_reason"))
+              .setPermissions(new String[] {requestedPermission});
       response.add("PLACEHOLDER_FOR_PERMISSION");
       response.add(permission);
     } else {
@@ -101,22 +97,21 @@ public class MyActionsApp extends DialogflowApp {
     return response.build();
   }
 
-
   @ForIntent("request_location_permission")
   public ActionResponse requestLocationPermission(ActionRequest request) {
     ResponseBuilder response = getResponseBuilder(request);
 
-    // If the request comes from a phone, we can't use coarse location.
-    String requestedPermission = request.hasCapability(Capability.SCREEN_OUTPUT.getValue()) ?
-        ConstantsKt.PERMISSION_DEVICE_PRECISE_LOCATION
-        : ConstantsKt.PERMISSION_DEVICE_COARSE_LOCATION;
+    // 'DEVICE_COARSE_LOCATION' will provide a street address.
+    // ['DEVICE_PRECISE_LOCATION'](https://developers.google.com/actions/assistant/helpers#user_information) can be used for geolocation.
+    String requestedPermission = ConstantsKt.PERMISSION_DEVICE_COARSE_LOCATION;
 
     response.getConversationData().put(DATA_KEY_REQUESTED_PERMISSION, requestedPermission);
 
     if (!request.getUserStorage().containsKey(STORAGE_KEY_LOCATION)) {
-      Permission permission = new Permission()
-          .setContext(formatResponse("permission_reason"))
-          .setPermissions(new String[]{requestedPermission});
+      Permission permission =
+          new Permission()
+              .setContext(formatResponse("permission_reason"))
+              .setPermissions(new String[] {requestedPermission});
       response.add("PLACEHOLDER_FOR_PERMISSION");
       response.add(permission);
     } else {
@@ -125,7 +120,6 @@ public class MyActionsApp extends DialogflowApp {
 
     return response.build();
   }
-
 
   @ForIntent("handle_permission")
   public ActionResponse handlePermission(ActionRequest request) {
@@ -141,8 +135,8 @@ public class MyActionsApp extends DialogflowApp {
 
     Map<String, Object> storage = response.getUserStorage();
 
-    String requestedPermission = (String) request.getConversationData()
-        .get(DATA_KEY_REQUESTED_PERMISSION);
+    String requestedPermission =
+        (String) request.getConversationData().get(DATA_KEY_REQUESTED_PERMISSION);
     if (requestedPermission.equals(ConstantsKt.PERMISSION_NAME)) {
       String name = request.getUser().getProfile().getDisplayName();
       storage.put(STORAGE_KEY_NAME, name);
@@ -151,31 +145,10 @@ public class MyActionsApp extends DialogflowApp {
       return response.build();
     }
     if (requestedPermission.equals(ConstantsKt.PERMISSION_DEVICE_COARSE_LOCATION)) {
-      // If we requested coarse location, it means that we're on a speaker device.
       String location = request.getDevice().getLocation().getCity();
       storage.put(STORAGE_KEY_LOCATION, location);
       showLocationOnScreen(request, response);
       return response.build();
-    }
-    if (requestedPermission.equals(ConstantsKt.PERMISSION_DEVICE_PRECISE_LOCATION)) {
-      // If we requested precise location, it means that we're on a phone.
-      // Because we will get only latitude and longitude, we need to
-      // reverse geocode to get the city.
-      LatLng latLng = request.getDevice().getLocation().getCoordinates();
-
-      try {
-        String city = coordinatesToCity(latLng.getLatitude(), latLng.getLongitude());
-        String key = STORAGE_KEY_LOCATION;
-        request.getUserStorage().put(key, city);
-        response.getUserStorage().put(key, city);
-        showLocationOnScreen(request, response);
-        return response.build();
-      } catch (Exception e) {
-        LOGGER.error(e.toString());
-        response.add(formatResponse("read_mind_error"));
-        response.endConversation();
-        return response.build();
-      }
     }
 
     LOGGER.error("Unrecognized permission");
@@ -189,28 +162,6 @@ public class MyActionsApp extends DialogflowApp {
     ResponseBuilder response = getResponseBuilder(request);
     sayLocation(response, (String) request.getUserStorage().get(STORAGE_KEY_LOCATION));
     return response.build();
-  }
-
-  /**
-   * Gets the city name from results returned by Google Maps reverse geocoding from coordinates.
-   */
-  private String coordinatesToCity(double latitude, double longitude) throws Exception {
-    GeoApiContext context = new GeoApiContext.Builder()
-        .apiKey(retrieveMapsKey())
-        .build();
-
-    com.google.maps.model.LatLng latLng = new com.google.maps.model.LatLng(latitude, longitude);
-
-    GeocodingResult[] results = GeocodingApi.reverseGeocode(context, latLng).await();
-    AddressComponent[] components = results[0].addressComponents;
-    for (AddressComponent component : components) {
-      for (AddressComponentType type : component.types) {
-        if (type.toString().equals("locality")) {
-          return component.longName;
-        }
-      }
-    }
-    throw new Exception("Could not parse city name from Google Maps results");
   }
 
   /**
@@ -232,10 +183,11 @@ public class MyActionsApp extends DialogflowApp {
       return;
     }
 
-    NewSurface newSurface = new NewSurface()
-        .setContext(formatResponse("new_surface_context"))
-        .setNotificationTitle(formatResponse("notification_text"))
-        .setCapabilities(Collections.singletonList(capability));
+    NewSurface newSurface =
+        new NewSurface()
+            .setContext(formatResponse("new_surface_context"))
+            .setNotificationTitle(formatResponse("notification_text"))
+            .setCapabilities(Collections.singletonList(capability));
     response.add("PLACEHOLDER_FOR_NEW_SURFACE");
     response.add(newSurface);
   }
@@ -243,12 +195,12 @@ public class MyActionsApp extends DialogflowApp {
   private void sayLocation(ResponseBuilder response, String city) {
     String key = retrieveMapsKey();
     String url =
-        "https://maps.googleapis.com/maps/api/staticmap?size=640x640&key=" + key + "&center="
+        "https://maps.googleapis.com/maps/api/staticmap?size=640x640&key="
+            + key
+            + "&center="
             + city;
     response.add(formatResponse("say_location", city));
-    Image image = new Image()
-        .setUrl(url)
-        .setAccessibilityText("City Map");
+    Image image = new Image().setUrl(url).setAccessibilityText("City Map");
     response.add(image);
     response.endConversation();
   }
